de53f9f98921476b7adc8c33aeece10fc2604ba3
soomtong <soomtong@gmail.com>
2016-02-19 22:55:30 +0900

    refactored menu array

--- index.js
+++ index.js
@@ -16,9 +16,11 @@ require('crash-reporter').start({
 let mainWindow = null;
 
 function onClosed() {
-  // dereference the window
-  // for multiple windows store them in an array
   mainWindow = null;
+}
+
+function cleanUpCache(item, focusedWindow) {
+  console.log('Cleaned up', item, focusedWindow);
 }
 
 function createMainWindow() {
@@ -38,90 +40,34 @@ function createMainWindow() {
     setOptsForDualScreen(opts);
   }
 
-  const win = new BrowserWindow(opts);
-/*
-  if (process.env.DEV) {
-    win.loadUrl('http://localhost:8000/dev.html');
-    win.openDevTools();
-  } else {
-    win.loadUrl(`file://${__dirname}/index.html`);
-  }
-*/
-  win.loadURL(`file://${__dirname}/index.html`);
-  win.on('closed', onClosed);
+  const menu = defaultMenu();
 
-  var menu = defaultMenu();
-  menu.splice(2, 0, {
+  var toolMenu = {
     label: 'Tools',
     submenu: [
       {
         label: 'Clean Cache',
-        click: function (item, focusedWindow) {
-          console.log('Cleaned up');
-        }
+        click: cleanUpCache
       }
     ]
-  });
+  };
 
-  // Set top-level application menu, using modified template
-  Menu.setApplicationMenu(Menu.buildFromTemplate(menu));
+  Menu.setApplicationMenu(Menu.buildFromTemplate([...menu.slice(0, 2), toolMenu, ...menu.slice(2)]));
+
+  const win = new BrowserWindow(opts);
+
+  win.loadURL(`file://${__dirname}/index.html`);
+  win.on('closed', onClosed);
 
   return win;
 }
 
-/*
-app.on('ready', function () {
-  const width = 800;
-  const height = 540;
-  const is2nd = process.argv.indexOf('--2nd') >= 0;
-
-  var opts = {
-    width: width,
-    height: height,
-    minWidth: 640,
-    minHeight: 420,
-    resizable: true
-  };
-
-  if (is2nd) {
-    setOptsForDualScreen(opts);
-  }
-
-  mainWindow = new BrowserWindow(opts);
-
-  // WARN: Inspect window should be opened before loading URL
-  //mainWindow.inspectElement(0, 0);
-
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
-  mainWindow.on('closed', function () {
-    // de ref the window
-    // for multiple windows store them in an array
-    mainWindow = null;
-  });
-
-  var menu = defaultMenu();
-  menu.splice(2, 0, {
-    label: 'Tools',
-    submenu: [
-      {
-        label: 'Clean Cache',
-        click: function (item, focusedWindow) {
-          console.log('Cleaned up');
-        }
-      }
-    ]
-  });
-
-  // Set top-level application menu, using modified template
-  Menu.setApplicationMenu(Menu.buildFromTemplate(menu));
-
-});
-*/
-
 function setOptsForDualScreen(opts) {
   var atomScreen = require('screen');
+
   var displays = atomScreen.getAllDisplays();
   var d2 = displays.length > 1 ? displays[1] : null;
+
   if (d2) {
     opts.x = d2.bounds.x + (d2.size.width - opts.width) / 2;
     opts.y = d2.bounds.y + (d2.size.height - opts.height) / 2;
